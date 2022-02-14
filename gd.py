10d54cee98eeceeee0f96530e91d0156b29ef96b
arod40 <arodacademic@gmail.com>
2022-02-14 16:22:10 -0600

    adjust lr

--- gd.py
+++ gd.py
@@ -6,20 +6,16 @@ random.seed(0)
 time_start = datetime.now()
 
 
-def update_lr(it, f_history, lr):
-    return lr
+def gd_step(w, lr, gradient):
+    sub_elem_wise = lambda vec1, vec2: [x - y for x, y in zip(vec1, vec2)]
+    scalar_mult = lambda scalar, vector: [scalar * x for x in vector]
+    return sub_elem_wise(w, scalar_mult(lr, gradient))
 
 
 def check_bounds(w, lower, upper):
     return all([l <= wi for l, wi in zip(lower, w)]) and all(
         [wi <= u for u, wi in zip(upper, w)]
     )
-
-
-def gd_step(w, lr, gradient):
-    sub_elem_wise = lambda vec1, vec2: [x - y for x, y in zip(vec1, vec2)]
-    scalar_mult = lambda scalar, vector: [scalar * x for x in vector]
-    return sub_elem_wise(w, scalar_mult(lr, gradient))
 
 
 MAX_QUERIES = 10000
@@ -30,33 +26,38 @@ d = int(input())
 lower = list(map(float, input().split()))
 upper = list(map(float, input().split()))
 
-lr = 0.1
+lr = 10
 
 # Initial value of w chosen at random from the set of valid values
 w = [random.random() * (w - l) + l for l, w in zip(lower, upper)]
-history = []
+w_query = w
+gradient = [0] * d
+last_f = None
 
 it = 0
 while True:
     it += 1
 
+    # Shrinking the lr to make sure the query falls within the bounds
+    w_query = gd_step(w, lr, gradient)
+    while not check_bounds(w_query, lower, upper):
+        lr /= 10
+        w_query = gd_step(w, lr, gradient)
+
     # Querying the grader
-    print(" ".join([str(wi) for wi in w]))
+    print(" ".join([str(wi) for wi in w_query]))
     f = float(input())
     gradient = list(map(float, input().split()))
 
-    # GD step
-    w = gd_step(w, lr, gradient)
-
-    # Updating history and lr
-    history.append((f, gradient))
-    lr = update_lr(it, history, lr)
+    # Updating lr and w
+    if last_f is None or f < last_f:
+        last_f = f
+        w = w_query
+    # Shrinking the lr in case it has overstepped
+    else:
+        lr /= 10
 
     # Checking for breaking conditions
-    if (
-        it == MAX_QUERIES
-        or not check_bounds(w, lower, upper)
-        or (datetime.now() - time_start).seconds > 1.9
-    ):
+    if it == MAX_QUERIES or (datetime.now() - time_start).seconds > 1.9:
         break
 
