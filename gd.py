8439b7a96cc4f891f943af90ea06c1298121c8a7
arod40 <arodacademic@gmail.com>
2022-02-25 13:55:29 -0600

    do RMSprop and decay lr if function value increases

--- gd.py
+++ gd.py
@@ -1,21 +1,24 @@
-import random
 from datetime import datetime
+from random import random
+from math import sqrt, inf
 
-random.seed(0)
 
 time_start = datetime.now()
 
 
-def gd_step(w, lr, gradient):
-    sub_elem_wise = lambda vec1, vec2: [x - y for x, y in zip(vec1, vec2)]
-    scalar_mult = lambda scalar, vector: [scalar * x for x in vector]
-    return sub_elem_wise(w, scalar_mult(lr, gradient))
-
-
-def check_bounds(w, lower, upper):
+def check_bounds(w, lower=None, upper=None):
+    lower = lower or [-1] * len(w)
+    upper = upper or [1] * len(w)
     return all([l <= wi for l, wi in zip(lower, w)]) and all(
         [wi <= u for u, wi in zip(upper, w)]
     )
+
+
+def normalization_functions(lows, ups):
+    halfs = [(up - low) / 2 for up, low in zip(lows, ups)]
+    norm = lambda w: [(x - low - half) / half for x, low, half in zip(w, halfs, lows)]
+    norm_inv = lambda w: [x * half + half + low for x, low, half in zip(w, halfs, lows)]
+    return norm, norm_inv
 
 
 MAX_QUERIES = 10000
@@ -26,38 +29,59 @@ d = int(input())
 lower = list(map(float, input().split()))
 upper = list(map(float, input().split()))
 
-lr = 10
+lr = 0.1
+eps = 1e-8
+beta = 0.9
+f_prev = inf
+w_prev = None
 
-# Initial value of w chosen at random from the set of valid values
-w = [random.random() * (w - l) + l for l, w in zip(lower, upper)]
-w_query = w
+w = [random() * (u - l) + l for u, l in zip(upper, lower)]
 gradient = [0] * d
-last_f = None
+v = 0
+
+norm, norm_inv = normalization_functions(lower, upper)
+elem_wise = lambda vec1, vec2, op: [op(x, y) for x, y in zip(vec1, vec2)]
+scalar_mult = lambda scalar, vector: [scalar * x for x in vector]
+mult = lambda x, y: x * y
+sub = lambda x, y: x - y
 
 it = 0
-while True:
+while it < MAX_QUERIES:  # and (datetime.now() - time_start).seconds < 1.9:
     it += 1
 
-    # Shrinking the lr to make sure the query falls within the bounds
-    w_query = gd_step(w, lr, gradient)
-    while not check_bounds(w_query, lower, upper):
-        lr /= 10
-        w_query = gd_step(w, lr, gradient)
+    # Unnormalizing
+    # w = norm_inv(w)
 
     # Querying the grader
-    print(" ".join([str(wi) for wi in w_query]))
+    print(" ".join([str(wi) for wi in w]))
     f = float(input())
     gradient = list(map(float, input().split()))
 
+    # Normalizing
+    # w = norm(w)
+
+    # Shrink lr if overstepped
+    if f > f_prev and it > 10:
+        lr /= 10
+        w = w_prev
+
     # Updating lr and w
-    if last_f is None or f < last_f:
-        last_f = f
-        w = w_query
-    # Shrinking the lr in case it has overstepped
-    else:
+    v = beta * v + (1 - beta) * sum(elem_wise(w, w, mult))
+    checked = False
+    # Adjusting learning rate if stepped out of the bounds
+    for i in range(10):
+        w = elem_wise(w, scalar_mult(lr / (sqrt(v) + eps), gradient), sub)
+        if check_bounds(w, lower, upper):
+            checked = True
+            break
         lr /= 10
 
-    # Checking for breaking conditions
-    if it == MAX_QUERIES or (datetime.now() - time_start).seconds > 1.9:
-        break
-
+    # If never got within bounds or change is less than epsilon, start over from another point
+    if not checked or abs(f_prev - f) < eps:
+        w = [random() * (u - l) + l for u, l in zip(upper, lower)]
+        f_prev = inf
+        w_prev = None
+        lr = 0.1
+    else:
+        f_prev = f
+        w_prev = w
